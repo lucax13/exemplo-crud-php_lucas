aa07bd7ace370e9560334d34985d75958c5ef612
lucax13 <lucas13lima2006@gmail.com>
2025-03-14 10:54:16 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -1,10 +1,17 @@
 <?php
 $servidor = "localhost";
 $usuario = "root";
-$ssenha = "";
+$senha = "";
 $banco = "vendas";
 
-$conexao = new PDO(
-    "mysql:host=$servidor;dbname=$banco;charset=utf8",
-    $usuario, $senha 
-);
+try{
+
+    $conexao = new PDO(
+        "mysql:host=$servidor;dbname=$banco;charset=utf8",
+        $usuario, $senha  
+    );
+
+    $conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} catch (Exception $erro){
+    die("Deu ruim:".$erro->getMessage());
+}
